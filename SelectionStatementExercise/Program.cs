bbb0f1bf4198be8ae260263b5b07ac2f3db74d11
yvonnehuynh79 <yvonnehuynh888@gmail.com>
2023-04-12 11:30:47 -0700

    first commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 5);
+            Console.WriteLine("try to guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput == favNumber)
+            {
+                Console.WriteLine("Perfect!");
+
+            }
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            
+                   
+
+            
+            
         }
     }
+
+
 }
